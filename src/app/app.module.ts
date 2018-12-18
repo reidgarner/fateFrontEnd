31b3efd6585d5f45e9bc03b19bf08eae60ed3fd9
reidgarner <reidbgarner@gmail.com>
2018-12-18 11:06:32 -0700

    Navbar working and styled. Signup and Login components generated.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,15 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavComponent } from './nav/nav.component';
+import { LoginComponent } from './login/login.component';
+import { SignupComponent } from './signup/signup.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavComponent
+    NavComponent,
+    LoginComponent,
+    SignupComponent
   ],
   imports: [
     BrowserModule,
