213e9882d127267a1266e732ac15d476c24b6360
Brumz <bmanrumsey24@gmail.com>
2018-12-19 14:18:23 -0700

    created a get all user data request

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, ChangeDetectionStrategy } from "@angular/core";
+import { GetAccountUsers } from "./getaccountUsers.service";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.scss"]
 })
 export class AppComponent {
-  title = 'fateFrontEnd';
+  title = "fateFrontEnd";
+  constructor(private _GetAccountUsers: GetAccountUsers) {}
+  ngOnInit() {
+    this._GetAccountUsers.getData().subscribe(data => {
+      console.log(data);
+    });
+  }
 }
