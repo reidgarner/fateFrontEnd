41d18fc667cb9032051871d441fede8db8b0ef4d
Bethany <bethany@Backbones-MacBook-Pro-2.local>
2019-02-06 10:24:22 -0700

    cleaned up code by removing some semicolons

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,10 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
-import { HomeComponent } from './home/home.component';
-import { SignupComponent } from './signup/signup.component';
-import { LoginComponent } from './login/login.component';
-import { ChartsComponent } from './charts/charts.component';
-import { ProfileComponent } from './profile/profile.component';
+import { NgModule } from '@angular/core'
+import { Routes, RouterModule } from '@angular/router'
+import { HomeComponent } from './home/home.component'
+import { SignupComponent } from './signup/signup.component'
+import { LoginComponent } from './login/login.component'
+import { ChartsComponent } from './charts/charts.component'
+import { ProfileComponent } from './profile/profile.component'
 
 const routes: Routes = [
   { path: '', component: HomeComponent},
@@ -13,7 +13,7 @@ const routes: Routes = [
   { path: 'profileResult', component: ChartsComponent},
   { path: 'login', component: LoginComponent},
   { path: 'profile', component: ProfileComponent}
-];
+]
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
